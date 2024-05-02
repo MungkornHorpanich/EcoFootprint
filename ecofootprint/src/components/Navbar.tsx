48846dcebe4c0ecf8fd40f577e4ec7b32dd0b77f
M <MungkornHorpanich@gmail.com>
2024-05-02 14:49:39 +0700

    Chaning the background color

--- ecofootprint/src/components/Navbar.tsx
+++ ecofootprint/src/components/Navbar.tsx
@@ -2,10 +2,10 @@ import { FC } from "react";
 
 const Navbar: FC = () => {
   return (
-    <div className="w-full p-1 m-0 mx-auto fixed backdrop-blur-md">
+    <div className="w-full p-1 m-0 mx-auto fixed">
       <div className="mx-auto max-w-3xl border border-gray-300 m-3 rounded-full">
         <div className="text-black flex flex-row justify-between p-4">
-          <h1 className="text-xl ml-1">🌎 EcoFootprint</h1>
+          <h1 className="text-lg ml-1">🌎 EcoFootprint</h1>
         </div>
       </div>
     </div>
