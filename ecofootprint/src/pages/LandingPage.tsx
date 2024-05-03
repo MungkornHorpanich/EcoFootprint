4de408b39a22d5d7ca6acba53196f56fa126af77
M <MungkornHorpanich@gmail.com>
2024-05-03 19:04:41 +0700

    Making a masthead

--- ecofootprint/src/pages/LandingPage.tsx
+++ ecofootprint/src/pages/LandingPage.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "../components/Navbar";
+import Masthead from "../components/masthead";
 
 const LandingPage: React.FC = () => {
   return (
     <div>
       <Navbar />
+      <Masthead />
     </div>
   );
 };
