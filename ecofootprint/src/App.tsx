e7f4f521b1ac5e77279e15bc64bd1757ae3dd9b5
M <MungkornHorpanich@gmail.com>
2024-05-05 12:27:29 +0700

    Adding the test page

--- ecofootprint/src/App.tsx
+++ ecofootprint/src/App.tsx
@@ -1,11 +1,16 @@
 import React from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
+import Test from "./pages/Test";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <LandingPage />,
+  },
+  {
+    path: "/test",
+    element: <Test />,
   },
 ]);
 
