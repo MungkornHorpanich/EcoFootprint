ed31d49a44f1eac02bd71d364626c90c6b16643f
M <MungkornHorpanich@gmail.com>
2024-05-09 20:09:22 +0700

    Adding the react-spinners

--- ecofootprint/src/pages/LandingPage.tsx
+++ ecofootprint/src/pages/LandingPage.tsx
@@ -1,16 +1,14 @@
-import React, { useRef } from "react";
+import React from "react";
 import Navbar from "../components/Navbar";
 import Masthead from "../components/masthead";
 import Faq from "../components/Faq";
 
 const LandingPage: React.FC = () => {
-  const faqRef = useRef<HTMLDivElement | null>(null);
-
   return (
     <div>
       <Navbar />
       <Masthead />
-      <Faq faqRef={faqRef} />
+      <Faq />
     </div>
   );
 };
