8a4a189b3b981064cdee05d345e441d63678f186
M <MungkornHorpanich@gmail.com>
2024-05-26 12:50:51 +0700

    Adding the globalStateContext.tsx

--- ecofootprint/src/pages/Calculation.tsx
+++ ecofootprint/src/pages/Calculation.tsx
@@ -11,7 +11,6 @@ const Calculation: React.FC = () => {
     damping: 30,
     restDelta: 0.001,
   });
-
   const q1 = [
     { name: "Yes, I do.", footprint: 4.6 },
     { name: "Yes, I do but It's an electric car.", footprint: 0.4 },
@@ -21,12 +20,12 @@ const Calculation: React.FC = () => {
 
   const q2 = [
     { name: "4 Short-haul roundtrips.", footprint: 1.9 },
-    { name: "12 Short-hual roundtrips.", footprint: 5.5 },
-    { name: "1 Long-hual roundtrips.", footprint: 2.35 },
+    { name: "12 Short-haul roundtrips.", footprint: 5.5 },
+    { name: "1 Long-haul roundtrips.", footprint: 2.35 },
   ];
 
   const q3 = [
-    { name: "Yes, I'm vegetarion.", footprint: -1.4 },
+    { name: "Yes, I'm vegetarian.", footprint: -1.4 },
     { name: "Yes, I'm vegan.", footprint: -1.6 },
     { name: "I don't eat red meat.", footprint: -0.9 },
     { name: "No, I'm not", footprint: 1.2 },
@@ -46,8 +45,8 @@ const Calculation: React.FC = () => {
   ];
 
   const q6 = [
-    { name: "Yes, I do but not alot.", footprint: -0.25 },
-    { name: "Yes, I do but alot.", footprint: -0.5 },
+    { name: "Yes, I do but not a lot.", footprint: -0.25 },
+    { name: "Yes, I do but a lot.", footprint: -0.5 },
     { name: "No, I don't", footprint: 0.4 },
   ];
 
@@ -57,50 +56,24 @@ const Calculation: React.FC = () => {
     { name: "A lot (500$ a month)", footprint: 0.5 },
   ];
 
-  // Q1
   const [selected1, setSelected1] = useState(q1[0]);
-  const footprint1 = selected1.footprint;
-
-  // Q2
   const [selected2, setSelected2] = useState(q2[0]);
-  const footprint2 = selected2.footprint;
-
-  // Q3
   const [selected3, setSelected3] = useState(q3[0]);
-  const footprint3 = selected3.footprint;
-
-  // Q4
   const [selected4, setSelected4] = useState(q4[0]);
-  const footprint4 = selected4.footprint;
-
-  // Q5
   const [selected5, setSelected5] = useState(q5[0]);
-  const footprint5 = selected5.footprint;
-
-  // Q6
   const [selected6, setSelected6] = useState(q6[0]);
-  const footprint6 = selected6.footprint;
-
-  // Q7
   const [selected7, setSelected7] = useState(q7[0]);
-  const footprint7 = selected7.footprint;
-
-  const [csum, setCsum] = useState<number | any>(null);
 
   useEffect(() => {
-    let sum =
-      footprint1 +
-      footprint2 +
-      footprint3 +
-      footprint4 +
-      footprint5 +
-      footprint6 +
-      footprint7;
+    const sum =
+      selected1.footprint +
+      selected2.footprint +
+      selected3.footprint +
+      selected4.footprint +
+      selected5.footprint +
+      selected6.footprint +
+      selected7.footprint;
     setCsum(sum);
-    console.log("let ---------------------");
-    console.log(sum.toFixed(2));
-    console.log("state ---------------------");
-    console.log(csum);
   }, [
     selected1,
     selected2,
@@ -111,12 +84,25 @@ const Calculation: React.FC = () => {
     selected7,
   ]);
 
+  const [csum, setCsum] = useState<number>(
+    selected1.footprint +
+      selected2.footprint +
+      selected3.footprint +
+      selected4.footprint +
+      selected5.footprint +
+      selected6.footprint +
+      selected7.footprint,
+  );
+
   return (
     <div className="py-12 w-screen px-3">
       <motion.div
         className="z-100 fixed top-0 left-0 right-0 h-[10px] m-2 rounded-full bg-gradient-to-r from-emerald-400 to-cyan-400 origin-left"
         style={{ scaleX }}
       />
+      <div className="fixed right-0 bottom-0 m-5 py-4 px-8">
+        {csum.toFixed(2)}
+      </div>
       <div className="flex flex-col max-w-2xl gap-24 mx-auto mt-12">
         <div id="heading">
           <h1 className="font-bold text-3xl">
